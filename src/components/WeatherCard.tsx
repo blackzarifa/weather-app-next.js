e3084c8f0376b3a8ee887c8ff901d6bc5876eb09
Rafael <rafaelirades@gmail.com>
2024-09-11 22:09:49 -0300

    feat: placeholder WeatherCard

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
+interface WeatherData {
+  city: string;
+  temperature: number;
+  description: string;
+}
+
+interface WeatherCardProps {
+  data: WeatherData;
+}
+
+const WeatherCard: React.FC<WeatherCardProps> = ({ data }) => {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>{data.city}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Temperature: {data.temperature}°C</p>
+        <p>Description: {data.description}</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default WeatherCard;
