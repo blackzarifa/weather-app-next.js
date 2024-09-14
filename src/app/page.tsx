2d57ef26d973dbd1484567b803bff11a724b59e6
blackzarifa <rafaelirades@gmail.com>
2024-09-13 23:06:08 -0300

    refac: page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,15 @@ const Home: React.FC = () => {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <div className="w-full max-w-5xl flex justify-between items-center">
         <h1 className="text-4xl font-bold">Weather App</h1>
         <ThemeToggle />
       </div>
 
-      <WeatherCard data={mockData} />
+      <div className="py-24">
+        <WeatherCard data={mockData} />
+      </div>
     </main>
   );
 };
