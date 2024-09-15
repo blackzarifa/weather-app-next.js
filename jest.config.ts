badc01cf746224964be9ff1fd743b55c95111ca6
blackzarifa <rafaelirades@gmail.com>
2024-09-15 20:15:54 -0300

    setup jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/node_modules/@testing-library/jest-dom'],
+};
+
+export default config;
