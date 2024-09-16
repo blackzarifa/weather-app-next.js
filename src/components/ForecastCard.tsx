b4a9af3bf856f1d55434a08a64ec566149c62243
blackzarifa <rafaelirades@gmail.com>
2024-09-16 20:04:34 -0300

    feat: date localization

--- src/components/ForecastCard.tsx
+++ src/components/ForecastCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import Image from 'next/image';
 import { ForecastData } from '@/lib/api';
 import { useTranslations } from 'next-intl';
+import { format } from 'date-fns';
 
 interface ForecastCardProps {
   data: ForecastData;
@@ -10,14 +11,18 @@ interface ForecastCardProps {
 }
 
 const ForecastCard: React.FC<ForecastCardProps> = ({ data, tempUnit }) => {
-  const t = useTranslations('ForecastCard');
-  const locale = useTranslations('Locale');
+  const t = useTranslations('Locale');
+
   const temperature = tempUnit === 'C' ? data.temperature : data.temperatureFahrenheit;
+  const formattedDate = format(
+    new Date(data.date),
+    t('lang') === 'en' ? 'eee, MMM dd' : 'dd/MM/yyyy'
+  );
 
   return (
     <Card className="w-full">
       <CardContent className="flex flex-col items-center p-4">
-        <p className="font-semibold">{data.date}</p>
+        <p className="font-semibold">{formattedDate}</p>
         <Image
           src={`https://openweathermap.org/img/wn/${data.icon}@2x.png`}
           alt={data.description}
